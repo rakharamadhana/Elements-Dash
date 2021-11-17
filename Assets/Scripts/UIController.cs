daa207e2202bdaf41c43fa0d7de65c37906b78e0
Rakha Ramadhana <rakha.ramadhana@yahoo.com>
2021-11-17 21:48:49 +0800

    Initial commit

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIController : MonoBehaviour
+{
+    Player player;
+    TMP_Text distanceText;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").GetComponent<Player>();
+        distanceText = GameObject.Find("DistanceText").GetComponent<TMP_Text>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int distance = Mathf.FloorToInt(player.distance);
+        distanceText.text = distance + " M";
+    }
+}
